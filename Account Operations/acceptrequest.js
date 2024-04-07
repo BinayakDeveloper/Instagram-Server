8f6d44adf00a97c3868468d187a7b67479d15a1a
BinayakDeveloper <banjarajajabar@gmail.com>
2024-04-07 15:09:53 +0530

    Deploy Commit 20

--- Account Operations/acceptrequest.js
+++ Account Operations/acceptrequest.js
@@ -5,11 +5,43 @@ async function acceptrequest(req, res, userDatabase) {
   let friend = await userDatabase.findOne({ token: friendtoken });
 
   if (user !== null && friend !== null) {
-    await user.updateOne({ $pull: { followRequests: friendtoken } });
-    await user.updateOne({ $push: { followers: friendtoken } });
-    await friend.updateOne({ $push: { following: usertoken } });
+    let userUpdate1 = await user.updateOne({
+      $pull: { followRequests: friendtoken },
+    });
+    let userUpdate2 = await user.updateOne({
+      $push: { followers: friendtoken },
+    });
+    let friendUpdate1 = await friend.updateOne({
+      $push: { following: usertoken },
+    });
+    if (
+      userUpdate1.acknowledged &&
+      userUpdate2.acknowledged &&
+      friendUpdate1.acknowledged
+    ) {
+      res.json({ status: true, response: "Accepted" });
+      let notificationUpdate = await user.updateOne({
+        $push: {
+          notification: {
+            type: "follow",
+            friendToken: friend.token,
+            createdAt: new Date.now(),
+          },
+        },
+      });
 
-    res.json({ status: true, response: "Accepted" });
+      if (notificationUpdate.acknowledged) {
+        res.json({
+          status: true,
+          response: "Notification updated",
+        });
+      } else {
+        res.json({
+          status: false,
+          response: "Error while updating notification",
+        });
+      }
+    }
   } else {
     res.json({
       status: false,
--- Account Operations/followuser.js
+++ Account Operations/followuser.js
@@ -8,19 +8,62 @@ async function followuser(req, res, userDatabase) {
     let privateUser = friend.privateStatus;
 
     if (!privateUser) {
-      await user.updateOne({ $push: { following: friendtoken } });
-      await friend.updateOne({ $push: { followers: usertoken } });
-      res.json({
-        status: true,
-        response: "Followed successfully",
+      let userUpdate = await user.updateOne({
+        $push: { following: friendtoken },
       });
-    } else {
-      if (!friend.followRequests.includes(usertoken)) {
-        await friend.updateOne({ $push: { followRequests: usertoken } });
+      let friendUpdate = await friend.updateOne({
+        $push: { followers: usertoken },
+      });
+
+      if (userUpdate.acknowledged && friendUpdate.acknowledged) {
         res.json({
           status: true,
-          response: "Request sent",
+          response: "Followed successfully",
         });
+
+        let notificationUpdate = await friend.updateOne({
+          $push: {
+            notification: {
+              type: "follow",
+              friendToken: friend.token,
+              createdAt: new Date.now(),
+            },
+          },
+        });
+
+        if (notificationUpdate.acknowledged) {
+          res.json({
+            status: true,
+            response: "Notification updated",
+          });
+        } else {
+          res.json({
+            status: false,
+            response: "Error while updating notification",
+          });
+        }
+      } else {
+        res.json({
+          status: false,
+          response: "Unknown error occured",
+        });
+      }
+    } else {
+      if (!friend.followRequests.includes(usertoken)) {
+        let friendUpdate = await friend.updateOne({
+          $push: { followRequests: usertoken },
+        });
+        if (friendUpdate.acknowledged) {
+          res.json({
+            status: true,
+            response: "Request sent",
+          });
+        } else {
+          res.json({
+            status: false,
+            response: "Unknown error occured",
+          });
+        }
       }
     }
   } else {
